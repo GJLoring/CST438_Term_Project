88cf9e0b7cdf04ba5a607412f07811a36b4d3a44
EC2 Default User <ec2-user@ip-172-31-29-195.us-west-2.compute.internal>
2019-04-02 10:21:22 +0000

    Adding stub of sign in page

---
+++ stuff2see/app/controllers/main.rb
@@ -0,0 +1,15 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  def add_sight
+      #pass
+  end
+
+  def find_sight
+      #pass
+  end
+  
+  def rate_sight
+      #pass
+  end
+
+end
