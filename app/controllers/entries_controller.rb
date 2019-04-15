314360df2d6ae1cd1a4dd523648ec116dbefea07
EC2 Default User <ec2-user@ip-172-31-29-195.us-west-2.compute.internal>
2019-04-15 10:48:44 +0000

    Update

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -12,15 +12,43 @@ class EntriesController < ApplicationController
     end
     
     def sign_up
+        @name = params[:visitor_name]
+        
+        unless @name.blank?
+            @entry = Entry.create({:name => @name})
+        end
+        
+        @entries = Entry.all
     end
     
     def recover_password
+        @name = params[:visitor_name]
+        
+        unless @name.blank?
+            @entry = Entry.create({:name => @name})
+        end
+        
+        @entries = Entry.all
     end
     
     def add_sight
+        @name = params[:visitor_name]
+        
+        unless @name.blank?
+            @entry = Entry.create({:name => @name})
+        end
+        
+        @entries = Entry.all
     end
     
     def find_sight
+        @name = params[:visitor_name]
+        
+        unless @name.blank?
+            @entry = Entry.create({:name => @name})
+        end
+        
+        @entries = Entry.all
     end
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :entries
+  resources :bubbles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   get 'entries/sign_in' => 'entries#sign_in'
